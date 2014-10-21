f62ce12a20e49f0c93da4e6cccac63beff84d12f
Thomas Thune Hansen <tth@bellcom.dk>
2014-10-21 14:37:22 +0200

    updated/added modules

---
+++ public_html/profiles/decreto/modules/decreto/decreto_signup/decreto_first_login.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * decreto_first_login.tpl.php
+ */
+?>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse leo tellus, luctus in risus vel, sollicitudin maximus nulla. Nunc a hendrerit dolor, non commodo orci. Pellentesque sollicitudin felis sed justo ultrices vehicula. Phasellus purus felis, pulvinar at iaculis id, bibendum eu arcu. Sed id sapien laoreet, tempus enim sit amet, ullamcorper massa. Vivamus eget nisi ac nisl aliquet accumsan in elementum dui. Aenean tempor lorem eu molestie egestas. Etiam efficitur nisl nulla, sit amet fringilla libero commodo et. Etiam id mi lobortis arcu efficitur semper at ac justo.
+</p>
+<?php print $first_login_form ?>
