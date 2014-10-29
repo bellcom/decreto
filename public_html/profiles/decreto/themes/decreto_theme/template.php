01e46ceac797897859df3cfcf6b89a657d7b0781
Thomas Thune Hansen <tth@bellcom.dk>
2014-10-29 10:14:33 +0100

    sidebar was delted

--- public_html/profiles/decreto/themes/decreto_theme/template.php
+++ public_html/profiles/decreto/themes/decreto_theme/template.php
@@ -8,7 +8,8 @@
  * Implements theme_preprocess_html().
  */
 function decreto_theme_preprocess_html(&$variables) {
-  drupal_add_css('http://fonts.googleapis.com/css?family=Asap:700|Open+Sans:400,700', array('type' => 'external'));
+  drupal_add_css('//fonts.googleapis.com/css?family=Asap:700|Open+Sans:400,700', array('type' => 'external'));
+  drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css', array('type' => 'external'));
 }
 
 /**
