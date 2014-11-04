be6281dbee1e9a9ce690a9ac6adf32c490d9fc7c
Vagrant User <vagrant@waiter.serve>
2014-11-04 12:42:54 +0000

    3D button mixin generator and style file, added

--- public_html/profiles/decreto/modules/decreto/meetings/page-meetings-overview.tpl.php
+++ public_html/profiles/decreto/modules/decreto/meetings/page-meetings-overview.tpl.php
@@ -4,5 +4,5 @@
  * page-meetings-overview.tpl.php
  */
 ?>
-<a href="/admin/content/meetingeditor/add" class="btn btn-primary pull-right"><?php print t('Create new'); ?></a>
+<a href="/admin/content/meetingeditor/add" class="btn btn-info pull-right"><?php print t('Create new'); ?></a>
 <?php print $meetings_table; ?>
